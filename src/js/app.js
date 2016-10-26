48bc46b381e002d0ccf2cc8d6429853a74d28cf6
Marliana Lara <marliana.lara@gmail.com>
2016-10-25 20:11:54 -0400

    form validation

--- src/js/app.js
+++ src/js/app.js
@@ -21,15 +21,6 @@
     this.locale = 'US';
     this.tax = 1.0575;
 
-    this.addNewItem = function() {
-      this.allItems.push({
-        name: this.newItem.name,
-        price: this.newItem.price,
-        quantity: this.newItem.quantity,
-        color: this.newItem.color,
-        discount: this.newItem.discount
-      });
-    };
 
     this.itemName = function(item) {
       if (this.locale === 'UK' && item.name === 'waste basket') {
@@ -52,7 +43,6 @@
 
     this.toggleLocale = function() {
       var spinButton = document.getElementById('currency-button');
-
       if (this.locale === 'US') {
         this.locale = 'UK';
         spinButton.className = 'spin';
@@ -60,6 +50,24 @@
         this.locale = 'US';
         spinButton.className = '';
       }
+    };
+
+    this.addNewItem = function() {
+      this.allItems.push({
+        name: this.newItem.name,
+        price: this.newItem.price,
+        quantity: this.newItem.quantity,
+        color: this.newItem.color,
+        discount: this.newItem.discount
+      });
+      this.newItem = {
+        name: null,
+        price: null,
+        quantity: null,
+        color: null,
+        discount: null
+      };
+      this.newItemForm.$setPristine();
     };
   });
 })();
