167b7cb1b7bed08c2869e1d1f7df0e1f506faece
Marliana Lara <marliana.lara@gmail.com>
2016-10-28 09:57:21 -0400

    log user and login time

--- src/js/controllers/admin.controller.js
+++ src/js/controllers/admin.controller.js
@@ -1,8 +1,12 @@
 angular.module('shopular').controller('adminCtlr', ['userMgmt', function(userMgmt) {
-  this.username = userMgmt.currentUser();
-  // get admin and login time
+    this.user = {
+      username: null,
+      loginTime: null
+    }
+  this.username = this.user.username;
+  // var user = userMgmt.currentUser();
   // userMgmt.login(userMgmt.username);
-
-
+  this.user = userMgmt.login(this.username)
+  console.log(this.user);
 }]);
  
--- src/js/services/userMgmt.service.js
+++ src/js/services/userMgmt.service.js
@@ -4,14 +4,13 @@ angular.module('shopular').factory('userMgmt', function() {
   return {
     login: function(username) {
       _currentUser = {
-        name: username,
         loginTime: new Date()
       }
       return _currentUser;
     },
-    currentUser: function() {
-      return _currentUser;
-    }
+    // currentUser: function() {
+    //   return _currentUser;
+    // }
   }
 
 });
